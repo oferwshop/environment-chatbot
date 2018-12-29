e6e52a65a9d5f59c437e7d20e08691afcc2d6bbc
oferwshop <oferwshop@gmail.com>
2018-12-30 01:43:03 +0200

    jj

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const
   app = express().use(body_parser.json()); // creates express http server
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-
+const FACEBOOK_GRAPH_API_BASE_URL = 'https://graph.facebook.com/v2.6/';
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
