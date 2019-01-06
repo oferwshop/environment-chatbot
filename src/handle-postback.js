37b1124229d1df83e007712649cb06c04eaae360
oferwshop <oferwshop@gmail.com>
2019-01-07 01:05:33 +0200

    refactor

---
+++ src/handle-postback.js
@@ -0,0 +1,21 @@
+
+const callSendAPI = require('./call-send-api')
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+  let response;
+  
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = { "text": "Thanks!" }
+  } else if (payload === 'no') {
+    response = { "text": "Oops, try sending another image." }
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+}
+
+  module.exports = handlePostback
