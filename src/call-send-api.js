e65e5bd8b5118264ca75d31343460065477cf6ca
oferwshop <oferwshop@gmail.com>
2019-01-11 21:00:23 +0200

    debug

--- src/call-send-api.js
+++ src/call-send-api.js
@@ -12,6 +12,7 @@ function callSendAPI(sender_psid, response) {
     "message": response
   }
 
+  console.log("**** SENDING: ****", JSON.stringify(request_body))
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
