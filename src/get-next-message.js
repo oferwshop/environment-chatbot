cbe843ea6ded57e255a09cf3cdf885acc9722932
oferwshop <oferwshop@gmail.com>
2019-01-15 00:15:46 +0200

    bug

--- src/get-next-message.js
+++ src/get-next-message.js
@@ -47,7 +47,7 @@ const getDate = webhook_event => {
     const datetime = _.get(webhook_event, 'message.nlp.entities.datetime')
     if (datetime) {
         const val = _.get(datetime, '[0].values[0]')
-        const date = new Date(val.from.value || val.value).getDay() 
+        const date = new Date(_.get(val, 'from.value') || val.value).getDay() 
         switch (date){
             case 0:
                 return "weekday/sunday"
