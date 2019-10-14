439ac87451ebf30e98a2b19bb65f4b15bbc42969
Ofer K <oferk@earnix.com>
2019-10-14 21:24:01 +0300

    change text

--- src/button-sets.js
+++ src/button-sets.js
@@ -31,7 +31,7 @@ const signWaiver =  [
 ]
 
 const registerClass =  [
-  { "type": 'web_url', "url": 'http://wix.to/hcDaAuc',"title": "Book", "webview_height_ratio": "full" }
+  { "type": 'web_url', "url": 'http://wix.to/hcDaAuc',"title": "להרשמה לאימון", "webview_height_ratio": "full" }
 ]
 
 
