1241deb882a7fb1ae432194432e36ff883d4ba55
Ofer K <oferk@earnix.com>
2019-10-19 02:22:08 +0300

    c

--- src/app-state.js
+++ src/app-state.js
@@ -4,27 +4,12 @@ const conversations = {}
 const botDisablePeriod =  1000 * 60 * 30
 const expirationPeriod = 1000 * 60 * 30
 
-const shouldReEnableBot = (timestamp, botDisabledTS) => timestamp - botDisabledTS > botDisablePeriod
-
-const getConversation = webhook_event => conversations[webhook_event.recipient.id]
-
-const initConversation = webhook_event => {
-    _.set(conversations, webhook_event.recipient.id, {} )
-    setLastUserInput(webhook_event)
-    return getConversation(webhook_event)
-}
-
-const setLastUserInput = webhook_event =>
-  _.set(getConversation(webhook_event), 'lastUserInputTS', hasMids(webhook_event) ? _.get(conversation, 'lastUserInputTS'): webhook_event.timestamp )
-
-const hasMids = webhook_event => _.get(webhook_event, 'delivery.mids')
-
 const handleConversationState = (webhook_event) => {
   console.log("*** CONVERSATIONS: " + JSON.stringify(conversations))
 
   let conversation = getConversation(webhook_event) || initConversation(webhook_event)
 
-  const timeSinceLastUserInput = webhook_event.timestamp - lastUserInputTS
+  const timeSinceLastUserInput = webhook_event.timestamp - _.get(conversation, 'lastUserInputTS', 0)
   const converstationExpired = timeSinceLastUserInput > expirationPeriod
   if (converstationExpired) conversation = initConversation(webhook_event)
 
@@ -52,5 +37,20 @@ const setMainScriptStarted = (webhook_event, val) => {
     const conversation = getConversation(webhook_event)
     _.set(conversation, 'mainScriptStarted', val)
 }
+
+const shouldReEnableBot = (timestamp, botDisabledTS) => timestamp - botDisabledTS > botDisablePeriod
+
+const getConversation = webhook_event => conversations[webhook_event.recipient.id]
+
+const initConversation = webhook_event => {
+    _.set(conversations, webhook_event.recipient.id, {} )
+    setLastUserInput(webhook_event)
+    return getConversation(webhook_event)
+}
+
+const setLastUserInput = webhook_event =>
+  _.set(getConversation(webhook_event), 'lastUserInputTS', hasMids(webhook_event) ? _.get(conversation, 'lastUserInputTS'): webhook_event.timestamp )
+
+const hasMids = webhook_event => _.get(webhook_event, 'delivery.mids')
 
 module.exports = { initConversation, handleConversationState, isDisabled, getMainScriptStarted, setMainScriptStarted  }
