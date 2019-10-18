8a0198f6e1347ff8af70ce1ea642bb593b0dfdab
Ofer K <oferk@earnix.com>
2019-10-19 00:54:06 +0300

    fix bug

--- src/app-state.js
+++ src/app-state.js
@@ -6,11 +6,12 @@ const botDisablePeriod =  1000 * 15// * 60 * 5
 const shouldReEnableBot = (timestamp, botDisabledTS) => timestamp - botDisabledTS > botDisablePeriod
 
 const getConversation = webhook_event => conversations[webhook_event.recipient.id]
+const setConversation = webhook_event => _.set(conversations, webhook_event.recipient.id, {} )
 const handleConversationState = (webhook_event) => {
   console.log("*** CONVERSATIONS: " + JSON.stringify(conversations))
+  if (!getConversation(webhook_event)) setConversation(webhook_event)
 
-  const conversation = getConversation(webhook_event)
-  if (!conversation) _.set(conversations, webhook_event.recipient.id, {} )
+  const conversation = getConversation(webhook_event) 
   const hasMids = _.get(webhook_event, 'delivery.mids')
   const lastUserInputTS = _.get(conversation, 'lastUserInputTS')
   const timeSinceLastUserInput = webhook_event.timestamp - lastUserInputTS
