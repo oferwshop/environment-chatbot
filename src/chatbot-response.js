14563a0916ed939e92843744c2936e9120674faf
Ofer K <oferk@earnix.com>
2019-10-19 12:01:52 +0300

    initialType

--- src/chatbot-response.js
+++ src/chatbot-response.js
@@ -11,9 +11,10 @@ function getChatbotResponse(webhook_event, sender_psid) {
     if (isDisabled(webhook_event)) return console.log("*** BOT DISABLED ") 
     
     const initialType = getResponseType(webhook_event)
+    if (isTextInput(initialType) && !hasMids(webhook_event)) setEnglish(webhook_event, !isHebrew(webhook_event))
+
     const type = getActualType(initialType, webhook_event)
     console.log("**** Response Type: " + type)
-    if (isTextInput(type) && !hasMids(webhook_event)) setEnglish(webhook_event, !isHebrew(webhook_event))
 
     return type === 'thank-you' && getReplyAndEmail(webhook_event, 'thank-you', sender_psid, _.get(webhook_event, 'message.text'))
       || type === 'get-waiver' && getReply(webhook_event, 'get-waiver')
