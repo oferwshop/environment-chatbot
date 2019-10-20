16c097eb4b7768814930ec9326475a50705d03da
Ofer K <oferk@earnix.com>
2019-10-20 21:29:23 +0300

    sdfds

--- src/helpers.js
+++ src/helpers.js
@@ -29,7 +29,7 @@ const waiverWords = ['רשם','טופס','בריאות','להירשם','רשמ',
 
 const generalInfoWords = ['מה זה', "hat is", "seminar", "סמינר"]
 
-const giNoGiWords = ['ההבדל',"נו גי", "סוגי", "no gi", "the difference", "שני סוגי", "kinds of", "types of"]
+const giNoGiWords = ['הבדל',"נו גי", "סוגי", "no gi", "the difference", "שני סוגי", "kinds of", "types of"]
 
 const possibleEndWords = ['תודה', 'ok', 'אוקי', 'סבבה', 'מגניב', 'hank', 'bye']
 
