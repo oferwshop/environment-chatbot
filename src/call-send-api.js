29b114d4d3abd6930344a9b9844f4f057494a550
Ofer K <oferk@earnix.com>
2019-10-22 20:44:13 +0300

    ss

--- src/call-send-api.js
+++ src/call-send-api.js
@@ -4,34 +4,27 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN || process.env.PAGE_ACCE
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-  const responseArray = new Array()
-  if (response.first) responseArray.push(response.first)
-  if (response.second) responseArray.push(response.second)
-  if (!(response.first || response.second)) responseArray.push(response)
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
 
-    responseArray.forEach( (response) => {
-
-      let request_body = {
-        "recipient": {
-          "id": sender_psid
-        },
-        "message": response
-      }
-    
-      // Send the HTTP request to the Messenger Platform
-      request({
-        "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": { "access_token": PAGE_ACCESS_TOKEN },
-        "method": "POST",
-        "json": request_body
-      }, (err, res, body) => {
-        if (!err) {
-          console.log('message sent!')
-        } else {
-          console.error("Unable to send message:" + err);
-        }
-      }); 
-    })
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
 }
 
   module.exports = callSendAPI
