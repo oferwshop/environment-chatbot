0e0792c8e23648ed8c40cefc2c128dfc3856b0c4
Ofer K <oferk@earnix.com>
2019-10-30 23:30:52 +0200

    emptuy

--- src/button-sets-eng.js
+++ src/button-sets-eng.js
@@ -64,7 +64,7 @@ const buttonSets = {
   "kids-info": kidsInfo,
   "military-info": militaryInfo,
   "adults-info": adultsInfo,
-//  "more-info": moreInfo,
+  "more-info": [],
   "schedule-free-session": signWaiver,
   "schedule-free-week": signWaiver,
   "greetings-location": greetingsLocation,
