cda585069f2ac89956b01fa242b2d8070fd8e7d2
Ofer K <oferk@earnix.com>
2019-11-11 07:30:42 +0200

    *

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
  *
- *
+ **
  * Starter Project for Messenger Platform Quick Start Tutorial
  *
  * Use this project as the starting point for following the 
